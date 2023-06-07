ee5743980505d99f6ed877758f35b0524a2bc16f
g5becks <techstar.dev@hotmail.com>
2023-06-07 17:49:00 -0400

    feat(Models): :sparkles: add exchanges models

---
+++ lib/src/rest/models/exchanges.dart
@@ -0,0 +1,33 @@
+// Copyright 2023 Takin Profit. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+typedef Exchange = ({
+  String? acronym,
+  String? assetClass,
+  int? id,
+  String? locale,
+  String? mic,
+  String? name,
+  String? operatingMic,
+  String? participantId,
+  String? type,
+  String? url,
+});
+
+extension Exchanges on Exchange {
+  static Exchange fromMap(Map<String, dynamic> map) {
+    return (
+      acronym: map['acronym'],
+      assetClass: map['asset_class'],
+      id: map['id'],
+      locale: map['locale'],
+      mic: map['mic'],
+      name: map['name'],
+      operatingMic: map['operating_mic'],
+      participantId: map['participant_id'],
+      type: map['type'],
+      url: map['url'],
+    );
+  }
+}
