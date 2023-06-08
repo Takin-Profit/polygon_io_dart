ce341bc28a5d17b21f9796e83ff96658c0753848
g5becks <techstar.dev@hotmail.com>
2023-06-08 11:57:26 -0400

    style(Models): :art: update extensions financials

--- lib/src/rest/models/financials.dart
+++ lib/src/rest/models/financials.dart
@@ -22,28 +22,28 @@ _Data _fromMap(Map<String, dynamic> d) => (
 /// An individual financial data point.
 typedef DataPoint = _Data;
 
-extension Data_Point on DataPoint {
+extension dataPoint on DataPoint {
   static DataPoint fromMap(Map<String, dynamic> d) => _fromMap(d);
 }
 
 /// Contains exchange gains losses data for a cash flow statement.
 typedef ExchangeGainsLosses = _Data;
 
-extension Exchange_Gains_Losses on ExchangeGainsLosses {
+extension exchangeGainsLosses on ExchangeGainsLosses {
   static ExchangeGainsLosses fromMap(Map<String, dynamic> d) => _fromMap(d);
 }
 
 /// Contains net cash flow data for a cash flow statement.
 typedef NetCashFlow = _Data;
 
-extension Net_Cash_Flow on NetCashFlow {
+extension netCashFlow on NetCashFlow {
   static NetCashFlow fromMap(Map<String, dynamic> d) => _fromMap(d);
 }
 
 /// Contains net cash flow from financing activities data for a cash flow statement.
 typedef NetCashFlowFromFinancingActivities = _Data;
 
-extension NetCashFlow_FromFinancing_Activities
+extension netCashFlowFromFinancingActivities
     on NetCashFlowFromFinancingActivities {
   static NetCashFlowFromFinancingActivities fromMap(Map<String, dynamic> d) =>
       _fromMap(d);
@@ -56,21 +56,21 @@ typedef CashFlowStatement = ({
   NetCashFlowFromFinancingActivities? netCashFlowFromFinancingActivities,
 });
 
-extension CashFlow_Statement on CashFlowStatement {
+extension cashFlowStatement on CashFlowStatement {
   static CashFlowStatement fromMap(Map<String, dynamic> d) => (
         exchangeGainsLosses: d['exchange_gains_losses'] != null
-            ? Exchange_Gains_Losses.fromMap(
+            ? exchangeGainsLosses.fromMap(
                 d['exchange_gains_losses'] as Map<String, dynamic>,
               )
             : null,
         netCashFlow: d['net_cash_flow'] != null
-            ? Net_Cash_Flow.fromMap(
+            ? netCashFlow.fromMap(
                 d['net_cash_flow'] as Map<String, dynamic>,
               )
             : null,
         netCashFlowFromFinancingActivities:
             d['net_cash_flow_from_financing_activities'] != null
-                ? NetCashFlow_FromFinancing_Activities.fromMap(
+                ? netCashFlowFromFinancingActivities.fromMap(
                     d['net_cash_flow_from_financing_activities']
                         as Map<String, dynamic>,
                   )
@@ -81,24 +81,25 @@ extension CashFlow_Statement on CashFlowStatement {
 /// Contains comprehensive income loss data for comprehensive income.
 typedef ComprehensiveIncomeLoss = _Data;
 
-extension Comprehensive_IncomeLoss on ComprehensiveIncomeLoss {
+extension comprehensiveIncomeLoss on ComprehensiveIncomeLoss {
   static ComprehensiveIncomeLoss fromMap(Map<String, dynamic> d) => _fromMap(d);
 }
 
 /// Contains comprehensive income loss attributable to parent data for comprehensive income.
 typedef ComprehensiveIncomeLossAttributableToParent = _Data;
 
-extension ComprehensiveIncomeLoss_AttributableToParent
+extension comprehensiveIncomeLossAttributableToParent
     on ComprehensiveIncomeLossAttributableToParent {
   static ComprehensiveIncomeLossAttributableToParent fromMap(
-          Map<String, dynamic> d) =>
+    Map<String, dynamic> d,
+  ) =>
       _fromMap(d);
 }
 
 /// Contains other comprehensive income loss data for comprehensive income.
 typedef OtherComprehensiveIncomeLoss = _Data;
 
-extension Other_Comprehensive_IncomeLoss on OtherComprehensiveIncomeLoss {
+extension otherComprehensiveIncomeLoss on OtherComprehensiveIncomeLoss {
   static OtherComprehensiveIncomeLoss fromMap(Map<String, dynamic> d) =>
       _fromMap(d);
 }
@@ -110,23 +111,23 @@ typedef ComprehensiveIncome = ({
   OtherComprehensiveIncomeLoss? otherComprehensiveIncomeLoss
 });
 
-extension Comprehensive_Income on ComprehensiveIncome {
+extension comprehensiveIncome on ComprehensiveIncome {
   static ComprehensiveIncome fromMap(Map<String, dynamic> d) => (
         comprehensiveIncomeLoss: d['comprehensive_income_loss'] != null
-            ? Comprehensive_IncomeLoss.fromMap(
+            ? comprehensiveIncomeLoss.fromMap(
                 d['comprehensive_income_loss'] as Map<String, dynamic>,
               )
             : null,
         comprehensiveIncomeLossAttributableToParent:
             d['comprehensive_income_loss_attributable_to_parent'] != null
-                ? ComprehensiveIncomeLoss_AttributableToParent.fromMap(
+                ? comprehensiveIncomeLossAttributableToParent.fromMap(
                     d['comprehensive_income_loss_attributable_to_parent']
                         as Map<String, dynamic>,
                   )
                 : null,
         otherComprehensiveIncomeLoss: d['other_comprehensive_income_loss'] !=
                 null
-            ? Other_Comprehensive_IncomeLoss.fromMap(
+            ? otherComprehensiveIncomeLoss.fromMap(
                 d['other_comprehensive_income_loss'] as Map<String, dynamic>,
               )
             : null
@@ -136,35 +137,35 @@ extension Comprehensive_Income on ComprehensiveIncome {
 /// Contains basic earning per share data for an income statement.
 typedef BasicEarningsPerShare = _Data;
 
-extension BasicEarnings_PerShare on BasicEarningsPerShare {
+extension basicEarningsPerShare on BasicEarningsPerShare {
   static BasicEarningsPerShare fromMap(Map<String, dynamic> d) => _fromMap(d);
 }
 
 /// Contains cost of revenue data for an income statement.
 typedef CostOfRevenue = _Data;
 
-extension CostOf_Revenue on CostOfRevenue {
+extension costOfRevenue on CostOfRevenue {
   static CostOfRevenue fromMap(Map<String, dynamic> d) => _fromMap(d);
 }
 
 /// Contains gross profit data for an income statement.
 typedef GrossProfit = _Data;
 
-extension Gross_Profit on GrossProfit {
+extension grossProfit on GrossProfit {
   static GrossProfit fromMap(Map<String, dynamic> d) => _fromMap(d);
 }
 
 /// Contains operating expenses data for an income statement.
 typedef OperatingExpenses = _Data;
 
-extension Operating_Expenses on OperatingExpenses {
+extension operatingExpenses on OperatingExpenses {
   static OperatingExpenses fromMap(Map<String, dynamic> d) => _fromMap(d);
 }
 
 /// Contains revenues data for an income statement.
 typedef Revenues = _Data;
 
-extension Revenue on Revenues {
+extension revenue on Revenues {
   static Revenues fromMap(Map<String, dynamic> d) => _fromMap(d);
 }
 
@@ -177,30 +178,30 @@ typedef IncomeStatement = ({
   Revenues? revenues,
 });
 
-extension Income_Statement on IncomeStatement {
+extension incomeStatement on IncomeStatement {
   static IncomeStatement fromMap(Map<String, dynamic> d) => (
         basicEarningsPerShare: d['basic_earnings_per_share'] != null
-            ? BasicEarnings_PerShare.fromMap(
+            ? basicEarningsPerShare.fromMap(
                 d['basic_earnings_per_share'] as Map<String, dynamic>,
               )
             : null,
         costOfRevenue: d['cost_of_revenue'] != null
-            ? CostOf_Revenue.fromMap(
+            ? costOfRevenue.fromMap(
                 d['cost_of_revenue'] as Map<String, dynamic>,
               )
             : null,
         grossProfit: d['gross_profit'] != null
-            ? Gross_Profit.fromMap(
+            ? grossProfit.fromMap(
                 d['gross_profit'] as Map<String, dynamic>,
               )
             : null,
         operatingExpenses: d['operating_expenses'] != null
-            ? Operating_Expenses.fromMap(
+            ? operatingExpenses.fromMap(
                 d['operating_expenses'] as Map<String, dynamic>,
               )
             : null,
         revenues: d['revenues'] != null
-            ? Revenue.fromMap(
+            ? revenue.fromMap(
                 d['revenues'] as Map<String, dynamic>,
               )
             : null,
@@ -215,30 +216,30 @@ typedef Financials = ({
   IncomeStatement? incomeStatement,
 });
 
-extension Financial on Financials {
+extension financial on Financials {
   static Financials fromMap(Map<String, dynamic> d) => (
         balanceSheet: d['balance_sheet'] != null
             ? (d['balance_sheet'] as Map<String, dynamic>).map(
                 (key, value) => MapEntry(
                   key,
-                  Data_Point.fromMap(
+                  dataPoint.fromMap(
                     value as Map<String, dynamic>,
                   ),
                 ),
               )
             : null,
         cashFlowStatement: d['cash_flow_statement'] != null
-            ? CashFlow_Statement.fromMap(
+            ? cashFlowStatement.fromMap(
                 d['cash_flow_statement'] as Map<String, dynamic>,
               )
             : null,
         comprehensiveIncome: d['comprehensive_income'] != null
-            ? Comprehensive_Income.fromMap(
+            ? comprehensiveIncome.fromMap(
                 d['comprehensive_income'] as Map<String, dynamic>,
               )
             : null,
         incomeStatement: d['income_statement'] != null
-            ? Income_Statement.fromMap(
+            ? incomeStatement.fromMap(
                 d['income_statement'] as Map<String, dynamic>,
               )
             : null
@@ -259,14 +260,14 @@ typedef StockFinancial = ({
   String? startDate,
 });
 
-extension Stock_Financial on StockFinancial {
+extension stockFinancial on StockFinancial {
   static StockFinancial fromMap(Map<String, dynamic> d) => (
         cik: d['cik'],
         companyName: d['company_name'],
         endDate: d['end_date'],
         filingDate: d['filing_date'],
         financials: d['financials'] != null
-            ? Financial.fromMap(
+            ? financial.fromMap(
                 d['financials'] as Map<String, dynamic>,
               )
             : null,
