ebe8810459b3aba9cdf6fac78a83299bb3ba66b6
g5becks <techstar.dev@hotmail.com>
2023-06-08 12:35:36 -0400

    feat(Models): :sparkles: add split model

---
+++ lib/src/rest/models/splits.dart
@@ -0,0 +1,22 @@
+// Copyright 2023 Takin Profit. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/// Split contains data for a historical stock split,
+/// including the ticker symbol, the execution date,
+/// and the factors of the split ratio.
+typedef Split = ({
+  String? executionDate,
+  int? splitFrom,
+  int? splitTo,
+  String? ticker,
+});
+
+extension split on Split {
+  static Split fromJson(Map<String, dynamic> json) => (
+        executionDate: json['execution_date'],
+        splitFrom: json['split_from'],
+        splitTo: json['split_to'],
+        ticker: json['ticker'],
+      );
+}
